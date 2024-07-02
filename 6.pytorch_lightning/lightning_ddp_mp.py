e65b2581e7cf300e4ebe7cfeff8dc113b2d3cf37
Ign0reLee <dlwpdud@gmail.com>
2024-07-02 21:02:49 +0900

    update ddp with multi gpu

--- 6.pytorch_lightning/lightning_ddp_mp.py
+++ 6.pytorch_lightning/lightning_ddp_mp.py
@@ -1,4 +1,4 @@
-import torch
+import torch, os
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
 
@@ -39,10 +39,16 @@ class AutoencoderKL_MP(pl.LightningModule):
         self.dev0 = dev0
         self.dev1 = dev1
 
-        self.encoder = Encoder(in_channels, latent_channels).to(f'cuda:{self.dev0}')
-        self.decoder = Decoder(latent_channels, out_channels).to(f'cuda:{self.dev1}')
-        self.quant_conv = Conv3dLayer(2 * latent_channels, 2 * latent_channels, 1).to(f'cuda:{self.dev0}')
-        self.post_quant_conv = Conv3dLayer(latent_channels, latent_channels, 1).to(f'cuda:{self.dev1}')
+        self.encoder = Encoder(in_channels, latent_channels)
+        self.decoder = Decoder(latent_channels, out_channels)
+        self.quant_conv = Conv3dLayer(2 * latent_channels, 2 * latent_channels, 1)
+        self.post_quant_conv = Conv3dLayer(latent_channels, latent_channels, 1)
+    
+    def set_cuda(self):
+        self.encoder.to(f'cuda:{self.dev0}')
+        self.decoder.to(f'cuda:{self.dev1}')
+        self.quant_conv.to(f'cuda:{self.dev0}')
+        self.post_quant_conv.to(f'cuda:{self.dev1}')
 
     def encode(self, x):
         h, _, _ = self.encoder(x)
@@ -68,6 +74,7 @@ class AutoencoderKL_MP(pl.LightningModule):
         output = self(x)
         loss = nn.functional.mse_loss(output, y)
         self.log('train_loss', loss)
+        print('train_loss', loss)
         return loss
 
     def configure_optimizers(self):
@@ -76,7 +83,7 @@ class AutoencoderKL_MP(pl.LightningModule):
 class RandomDataset(Dataset):
     def __init__(self, size, length):
         self.len = length
-        self.data = torch.randn(length, size)
+        self.data = torch.randn(length, size, size)
 
     def __getitem__(self, index):
         return self.data[index], self.data[index]
@@ -97,43 +104,21 @@ class RandomDataModule(pl.LightningDataModule):
     def train_dataloader(self):
         return DataLoader(self.dataset, batch_size=self.batch_size)
 
+
 class CustomDDPStrategy(DDPStrategy):
-    def setup_environment(self):
+    def make_multiple_model(self):
         world_size = self.world_size
-        rank = self.local_rank
-        print("world_size",world_size, "rank",rank)
-
-        dist.init_process_group(
-            backend="nccl",
-            init_method="env://",
-            rank = self.local_rank,
-            world_size= self.world_size
-        )
-
-        group_0 = [0, 1]
-        group_1 = [2, 3]
-        if rank in group_0:
-            self.process_group = dist.new_group(ranks=group_0)
-        elif rank in group_1:
-            self.process_group = dist.new_group(ranks=group_1)
-        else:
-            raise ValueError(f"Rank {rank} is not in any group")
-
-        self.local_rank = rank % 2
-
-        print(f"Global rank {rank} in process group {self.process_group}, local rank {self.local_rank}")
-
-    def configure_ddp(self):
-        self.model = torch.nn.parallel.DistributedDataParallel(
-            self.model,
-            device_ids=[self.local_rank],
-            process_group=self.process_group
-        )
+        rank = self.local_rank        
+        self.local_rank_1 = (rank * 2) % (world_size*2)
+        self.local_rank_2 = (rank * 2 + 1) % (world_size * 2)
+        self.model.dev0 = self.local_rank_1
+        self.model.dev1 = self.local_rank_2
+        self.model.set_cuda()
 
     def setup(self, trainer):
-        self.setup_environment()
-        self.configure_ddp()
-        trainer.model = self.model
+        self.make_multiple_model()
+        super().setup(trainer=trainer)
+        print(self.global_rank, self.determine_ddp_device_ids())
 
 
 
@@ -143,9 +128,10 @@ if __name__ == "__main__":
 
     trainer = Trainer(
         strategy=CustomDDPStrategy(find_unused_parameters=False),
+        # strategy="ddp",
         accelerator="gpu",
-        devices=[0, 1],  # Specify all 4 GPUs
-        num_nodes=1,
+        devices=[0,2],  # Specify all 4 GPUs
+        # num_nodes=2,
         max_epochs=10,
     )
 
